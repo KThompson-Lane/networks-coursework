3d3a4f92974297d8df2e1b26d2b2b594314fa34b
KThompson-Lane <57400783+KThompson-Lane@users.noreply.github.com>
2023-02-22 16:48:57 +0000

    Add key exchange to main class

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,30 @@
+import Security.KeyExchanger;
+
 public class Main {
     public static void main(String[] args) {
         //TODO:  Take in port, IP and host as Args maybe?
         int portNum = 55555;
         String ipAddr = "192.168.0.18";
         boolean Host = false;
-        Connector connector = new Connector(portNum, ipAddr);
 
-        //1: Set up our connection as either host or client
-        //TODO: Make the connect as host return security information
+        //TODO: Consider merging connector and KeyExchanger class
+        Connector connector = new Connector(portNum, ipAddr);
+        KeyExchanger keyExchanger = new KeyExchanger(portNum, ipAddr);
+
+        long secretKey;
+
+        //1: Set up our connection as either host or client and establish a shared secret key
         if(Host)
+        {
             connector.ConnectAsHost();
-        else
+            //  After establishing connection, exchange keys
+            secretKey = keyExchanger.ExchangeAsHost();
+        }
+        else{
             connector.ConnectAsClient();
+            //  After establishing connection, exchange keys
+            secretKey = keyExchanger.ExchangeAsClient();
+        }
 
         //2: Set up our speak and listener threads
         //  New thread for listen, passing our port number
