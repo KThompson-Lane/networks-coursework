b0ea71e33b32c7f5c16347a29e1d1e876dc67b5f
KThompson-Lane <57400783+KThompson-Lane@users.noreply.github.com>
2023-02-28 10:53:24 +0000

    Added test methods to security layer for audio and number encryption

--- src/Security/SecurityLayer.java
+++ src/Security/SecurityLayer.java
@@ -1,6 +1,11 @@
 package Security;
 
+import java.io.IOException;
 import java.nio.ByteBuffer;
+import CMPC3M06.AudioPlayer;
+import CMPC3M06.AudioRecorder;
+
+import javax.sound.sampled.LineUnavailableException;
 
 public class SecurityLayer {
     public static byte[] EncryptData(byte[] plainText, long keyInput)
@@ -32,25 +37,57 @@ public class SecurityLayer {
         return decryptBuff.array();
     }
 
-    public static void main(String[] args) {
-        //TEST
-        long input = 123456;
-        long secretKey = 380031;
+    public static void main(String[] args)
+    {
+        try {
+            TestAudio(832139);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public static void TestNumber(int input, int key)
+    {
         ByteBuffer inputBuff = ByteBuffer.allocate(64);
         ByteBuffer cipherBuff = ByteBuffer.allocate(64);
         ByteBuffer outputBuff = ByteBuffer.allocate(64);
-
+        
         inputBuff.putLong(input);
-        byte[] cipherText = SecurityLayer.EncryptData(inputBuff.array(), secretKey);
+        byte[] cipherText = SecurityLayer.EncryptData(inputBuff.array(), key);
         cipherBuff.put(cipherText);
         long cipherInput = cipherBuff.getLong(0);
-        byte[] plainText = SecurityLayer.DecryptData(cipherText, secretKey);
+        byte[] plainText = SecurityLayer.DecryptData(cipherText, key);
         outputBuff.put(plainText);
         long decryptOutput = outputBuff.getLong(0);
 
         System.out.println("Input = " +input);
         System.out.println("Encrypted input = " + cipherInput);
         System.out.println("Decrypted output = " +decryptOutput);
-
+    }
+    public static void TestAudio(int key) throws IOException
+    {
+        boolean decrypt = false;
+        AudioRecorder recorder = null;
+        AudioPlayer player = null;
+        try{
+            recorder = new AudioRecorder();
+            player = new AudioPlayer();
+        } catch (LineUnavailableException e) {
+            System.out.println("ERROR: AudioSender: Could not start audio recorder.");
+            e.printStackTrace();
+            System.exit(0);
+        }
+        while(true)
+        {
+            byte[] block = recorder.getBlock();
+            byte[] encryptedAudio = SecurityLayer.EncryptData(block, key);
+            byte[] decryptedAudio = SecurityLayer.DecryptData(encryptedAudio, key);
+            if(decrypt)
+            {
+                player.playBlock(decryptedAudio);
+            }
+            else {
+                player.playBlock(encryptedAudio);
+            }
+        }
     }
 }
