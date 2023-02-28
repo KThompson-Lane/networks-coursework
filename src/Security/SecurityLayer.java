3ad245eae528d7847534ea8c2ba558ee89e2ab78
KThompson-Lane <57400783+KThompson-Lane@users.noreply.github.com>
2023-02-28 14:28:49 +0000

    separate encryption into its own class

--- src/Security/SecurityLayer.java
+++ src/Security/SecurityLayer.java
@@ -8,34 +8,6 @@ import CMPC3M06.AudioRecorder;
 import javax.sound.sampled.LineUnavailableException;
 
 public class SecurityLayer {
-    public static byte[] EncryptData(byte[] plainText, long keyInput)
-    {
-
-        ByteBuffer encryptBuff = ByteBuffer.allocate(plainText.length);
-        ByteBuffer plaintext = ByteBuffer.wrap(plainText);
-
-        for (int i = 0; i < (plainText.length/8); i++)
-        {
-            long dataSeg = plaintext.getLong();
-            dataSeg = dataSeg ^ keyInput;
-            encryptBuff.putLong(dataSeg);
-        }
-
-        return encryptBuff.array();
-    }
-
-    public static byte[] DecryptData(byte[] encryptedData, long keyInput)
-    {
-        ByteBuffer cipherText = ByteBuffer.wrap(encryptedData);
-        ByteBuffer decryptBuff = ByteBuffer.allocate(encryptedData.length);
-        for (int i = 0; i < (encryptedData.length/8); i++)
-        {
-            long encryptedSegment = cipherText.getLong();
-            encryptedSegment = encryptedSegment ^ keyInput;
-            decryptBuff.putLong(encryptedSegment);
-        }
-        return decryptBuff.array();
-    }
 
     public static void main(String[] args)
     {
@@ -52,10 +24,10 @@ public class SecurityLayer {
         ByteBuffer outputBuff = ByteBuffer.allocate(64);
         
         inputBuff.putLong(input);
-        byte[] cipherText = SecurityLayer.EncryptData(inputBuff.array(), key);
+        byte[] cipherText = SimpleEncryption.EncryptData(inputBuff.array(), key);
         cipherBuff.put(cipherText);
         long cipherInput = cipherBuff.getLong(0);
-        byte[] plainText = SecurityLayer.DecryptData(cipherText, key);
+        byte[] plainText = SimpleEncryption.DecryptData(cipherText, key);
         outputBuff.put(plainText);
         long decryptOutput = outputBuff.getLong(0);
 
@@ -79,8 +51,8 @@ public class SecurityLayer {
         while(true)
         {
             byte[] block = recorder.getBlock();
-            byte[] encryptedAudio = SecurityLayer.EncryptData(block, key);
-            byte[] decryptedAudio = SecurityLayer.DecryptData(encryptedAudio, key);
+            byte[] encryptedAudio = SimpleEncryption.EncryptData(block, key);
+            byte[] decryptedAudio = SimpleEncryption.DecryptData(encryptedAudio, key);
             if(decrypt)
             {
                 player.playBlock(decryptedAudio);
