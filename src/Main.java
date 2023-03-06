982832c3f1d8d70088d362646eed7cefa2e141d6
KThompson-Lane <57400783+KThompson-Lane@users.noreply.github.com>
2023-03-06 11:22:24 +0000

    add user input

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,52 @@
 import Security.KeyExchanger;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+
 public class Main {
     public static void main(String[] args) {
-        //TODO:  Take in port, IP and host as Args maybe?
-        int portNum = 55555;
-        String ipAddr = "192.168.168.0";
-        boolean Host = false;
+        InetAddress destinationAddress;
+        int destinationPort = 55555;
+        boolean Host;
 
-        Connector connector = new Connector(portNum, ipAddr);
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(System.in));
+
+        //  Get user to choose whether to run as the host or client
+        //  TODO: Get user to choose to enable encryption or not
+        try {
+            System.out.println("Please enter 'host' to run as the host device, or 'client' to run as the client device...");
+            switch (reader.readLine().toLowerCase())
+            {
+                case "host":
+                    System.out.println("Running as host!");
+                    Host = true;
+                    break;
+                default:
+                    System.out.println("Running as client!");
+                    Host = false;
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        //  Get user to enter destination address
+        while(true)
+        {
+            try{
+                System.out.println("Please enter the address of the device you want to connect to...");
+                String ipAddr =  reader.readLine();
+                destinationAddress = InetAddress.getByName(ipAddr);
+                System.out.println("Attempting connection with '"+ipAddr+"'...");
+                break;
+            }catch (IOException e) {
+                System.out.println("Invalid destination address...");
+            }
+        }
+
+        Connector connector = new Connector(destinationPort, destinationAddress.getHostName());
 
         //1: Set up our connection as either host or client
         if(Host)
@@ -17,7 +56,7 @@ public class Main {
 
         //  TODO: Consider merging connector and KeyExchanger class
         //  After establishing connection, determine shared secret key
-        KeyExchanger keyExchanger = new KeyExchanger(portNum, ipAddr);
+        KeyExchanger keyExchanger = new KeyExchanger(destinationPort, destinationAddress.getHostName());
         long secretKey;
         if(Host)
             secretKey = keyExchanger.ExchangeAsHost();
@@ -26,9 +65,9 @@ public class Main {
 
         //2: Set up our speak and listener threads
         //  New thread for listen, passing our port number and secret key
-        Listener listener = new Listener(portNum, secretKey);
+        Listener listener = new Listener(destinationPort, secretKey);
         //  New thread for speak, passing our IP address and port along with our secret key
-        Speaker speaker = new Speaker(portNum, ipAddr, secretKey);
+        Speaker speaker = new Speaker(destinationPort, destinationAddress.getHostName(), secretKey);
 
         //  Run these threads in a loop
         listener.Start();
